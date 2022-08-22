c87c2c41d4d39487a3c181f50ba7722702b44696
kurigaki <kenji.kurigaki@gmail.com>
2022-08-22 23:33:50 +0900

    決済処理を記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,12 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
     if @order.valid?
+      
+      Payjp::Charge.create(
+        amount: order_params[:price],  # 商品の値段
+        card: order_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
       @order.save
       return redirect_to root_path
     else
